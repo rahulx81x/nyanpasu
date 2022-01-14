20cf93aac9179e5e7ba48d6ed50fea3ad64c87a2
rahulx81x <rahulgouri072@gmail.com>
2022-01-14 20:36:56 +0530

    a bit of cosmetic changes

--- displaybox.py
+++ displaybox.py
@@ -3,15 +3,16 @@ from animechan import quote, by
 from openBrowser import search_anime
 
 qbox = Tk()
-text1 = Text(qbox, height=8, width=70)
+text1 = Text(qbox, height=10, width=60)
 text1.tag_configure("tag_name", justify='center')
 text1.insert("1.0", "\n".join(quote))
 text1.tag_add("tag_name", "1.0", "end")
-text1.config(wrap="word", font=("Franklin Gothic Medium", 12, "italic"))
+text1.config(wrap="word", font=("Franklin Gothic Medium", 24, "italic"))
 text1.grid(row=1,column=0)
 
 
 text2 = Label(qbox, text=by)
+text2.config(font=("Franklin Gothic Medium", 12, "italic"))
 text2.grid(row=2,column=0)
 
 anime_button = Button(qbox, text="Search Anime", command=search_anime)
